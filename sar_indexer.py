60ad988fadc753f37b18af40c65832a54b0c684a
Vicent Pérez <vicennt@gmail.com>
2017-05-07 12:14:16 +0200

    Create main

--- sar_indexer.py
+++ sar_indexer.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 #! -*- encoding: utf8 -*-
+
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3 or len(sys.argv) > 3:
+    	print("Usage: python sar_indexer.py <dir_news> <index_file>")
+    else: 
+    	dir_news = sys.argv[1]
+    	index_file = sys.argv[2]
+
+        
