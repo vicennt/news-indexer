aaf08c78ef4d53c0d6b3b13ea073d375bbed4f03
Vicent Pérez <vicennt@gmail.com>
2017-05-09 17:20:45 +0200

    Create load data funtion

--- sar_searcher.py
+++ sar_searcher.py
@@ -4,12 +4,21 @@
 import sys, os, re
 import pickle
 
+dictionary_terms = {}
+dictionary_docs = {}
+dictionary_news = {}
+
+
+def load_data(file):
+	global dictionary_terms, dictionary_docs, dictionary_news
+	dictionary_terms, dictionary_docs, dictionary_news = pickle.load(open(index_file,'rb'))
+
+
 
 def process_query(query):
 	print("Hi!")
+	print(dictionary_docs)
 	return query
-
-
 
 
 
@@ -23,8 +32,7 @@ if __name__ == "__main__":
     	if not os.path.exists(index_file):
     		print("Error: The file " + index_file + " doesn't exist")
     		sys.exit(1)
-    	dictionary_terms, dictionary_docs, dictionary_news = pickle.load(open(index_file,'rb'))
-    	print(dictionary_docs)
+    	load_data(index_file) 
     	while(True):
     		query = input("Enter your query: ")
     		if(not query):
